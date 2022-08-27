1bb82ba3fe88f1604747eb94f5744036ce38d3e9
Bob <vladimir@kashir.in>
2022-08-27 16:51:23 +0300

    i dont understend 48 hmmmm...

---
+++ toys.py
@@ -0,0 +1,20 @@
+full = False
+
+donations = []
+full_load = 45
+
+toys = ['robot', 'doll', 'ball', 'wolfer']
+
+while not full:
+    for toy in toys:
+        donations.append(toy)
+        size = len(donations)
+        if (size >= full_load):
+            full = True
+
+print('Full', len(donations), 'toys') #Full 48 toys
+
+print(donations) #['robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer', 'robot', 'doll', 'ball', 'wolfer']
+
+
+#я чомусь не зрозумів поки, як так, у фулл_лоад значення 45, а коли робиш Прінт лен(донатіонс) їх стає 48... хм...
